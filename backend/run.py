d189c96a77ff7b60fe4b2270f58a7a0126f0c751
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2021-07-12 15:19:27 -0300

    Creating web with create react app

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,16 @@
+import dotenv
+import dotenv
+
+dotenv_variables = dotenv.dotenv_values()
+PORT=dotenv_variables.get('PORT')
+
+if not PORT:
+    PORT = 3000
+
+
 from app import app
 
+
+
 if __name__ == "__main__":
-    app.run(port=3000)
+    app.run(port=PORT)
